479ab861079c3d911f7442d7c941a1cbc923ffdd
ChaeheeLeee <155871850+ChaeheeLeee@users.noreply.github.com>
2024-01-09 23:05:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, a, b;
+	cin >> N;
+	a = N / 5;
+
+	while (1) {
+		if (a < 0) {
+			cout << -1;
+			return 0;
+		}
+		if ((N - (5 * a)) % 3 == 0) {
+			b = (N - (5 * a)) / 3;
+			break;
+		}
+		a--;
+	}
+	cout << a + b;
+	return 0;
+}
