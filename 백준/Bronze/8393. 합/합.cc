a0cf2252d9500a70db42ade6a03f9d3f7433b9b2
ChaeheeLeee <155871850+ChaeheeLeee@users.noreply.github.com>
2024-01-12 06:00:18 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    long long n;
+    long long sum = 0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+    
+    return 0;
+}
